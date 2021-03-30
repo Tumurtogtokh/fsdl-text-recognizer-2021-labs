12a08bf30000e1e408e462f43290a0f239f54db3
tumurtogtokh <tumurtogtokh@gmail.com>
2021-03-30 22:47:44 +0100

    formatting

--- lab1/text_recognizer/lit_models/base.py
+++ lab1/text_recognizer/lit_models/base.py
@@ -11,7 +11,8 @@ ONE_CYCLE_TOTAL_STEPS = 100
 
 class BaseLitModel(pl.LightningModule):
     """
-    Generic PyTorch-Lightning class that must be initialized with a PyTorch module.
+    Generic PyTorch-Lightning class that must be initialized
+    with a PyTorch module.
     """
 
     def __init__(self, model, args: argparse.Namespace = None):
@@ -25,11 +26,12 @@ class BaseLitModel(pl.LightningModule):
         self.lr = self.args.get("lr", LR)
 
         loss = self.args.get("loss", LOSS)
-        if not loss in ("ctc", "transformer"):
+        if loss not in ("ctc", "transformer"):
             self.loss_fn = getattr(torch.nn.functional, loss)
 
         self.one_cycle_max_lr = self.args.get("one_cycle_max_lr", None)
-        self.one_cycle_total_steps = self.args.get("one_cycle_total_steps", ONE_CYCLE_TOTAL_STEPS)
+        self.one_cycle_total_steps = self.args.get("one_cycle_total_steps",
+                                                   ONE_CYCLE_TOTAL_STEPS)
 
         self.train_acc = pl.metrics.Accuracy()
         self.val_acc = pl.metrics.Accuracy()
@@ -37,24 +39,32 @@ class BaseLitModel(pl.LightningModule):
 
     @staticmethod
     def add_to_argparse(parser):
-        parser.add_argument("--optimizer", type=str, default=OPTIMIZER, help="optimizer class from torch.optim")
+        parser.add_argument("--optimizer", type=str, default=OPTIMIZER,
+                            help="optimizer class from torch.optim")
         parser.add_argument("--lr", type=float, default=LR)
         parser.add_argument("--one_cycle_max_lr", type=float, default=None)
-        parser.add_argument("--one_cycle_total_steps", type=int, default=ONE_CYCLE_TOTAL_STEPS)
-        parser.add_argument("--loss", type=str, default=LOSS, help="loss function from torch.nn.functional")
+        parser.add_argument("--one_cycle_total_steps", type=int,
+                            default=ONE_CYCLE_TOTAL_STEPS)
+        parser.add_argument("--loss", type=str, default=LOSS,
+                            help="loss function from torch.nn.functional")
         return parser
 
     def configure_optimizers(self):
         optimizer = self.optimizer_class(self.parameters(), lr=self.lr)
         if self.one_cycle_max_lr is None:
             return optimizer
-        scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer=optimizer, max_lr=self.one_cycle_max_lr, total_steps=self.one_cycle_total_steps)
-        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer=optimizer,
+            max_lr=self.one_cycle_max_lr,
+            total_steps=self.one_cycle_total_steps
+        )
+        return {"optimizer": optimizer, "lr_scheduler": scheduler,
+                "monitor": "val_loss"}
 
     def forward(self, x):
         return self.model(x)
 
-    def training_step(self, batch, batch_idx):  # pylint: disable=unused-argument
+    def training_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
         loss = self.loss_fn(logits, y)
@@ -63,15 +73,16 @@ class BaseLitModel(pl.LightningModule):
         self.log("train_acc", self.train_acc, on_step=False, on_epoch=True)
         return loss
 
-    def validation_step(self, batch, batch_idx):  # pylint: disable=unused-argument
+    def validation_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
         loss = self.loss_fn(logits, y)
         self.log("val_loss", loss, prog_bar=True)
         self.val_acc(logits, y)
-        self.log("val_acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val_acc", self.val_acc, on_step=False,
+                 on_epoch=True, prog_bar=True)
 
-    def test_step(self, batch, batch_idx):  # pylint: disable=unused-argument
+    def test_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
         self.test_acc(logits, y)
